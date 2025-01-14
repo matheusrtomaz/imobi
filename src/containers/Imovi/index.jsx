f22a8420c6371c108843920b75830a2a28ee8a73
Matheus Rodrigues Tomaz <157989074+matheusrtomaz@users.noreply.github.com>
2025-01-13 22:08:48 -0300

    TopBanner mid

--- src/containers/Imovi/index.jsx
+++ src/containers/Imovi/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Container } from './styles';
+import TopBanner from '../../components/TopBanner';
 
 const Imovi = () => {
   return (
     <Container>
+      <TopBanner />
         <p>Oi</p>
     </Container>
   )
